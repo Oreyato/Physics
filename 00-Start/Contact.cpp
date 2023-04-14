f665434230d7dfb63cc057b9f34dc352f3dca1ca
Oreyato <oreyato.pro@gmail.com>
2023-04-14 15:12:47 +0200

    feat: impulse linear

--- 00-Start/Contact.cpp
+++ 00-Start/Contact.cpp
@@ -6,12 +6,22 @@ void Contact::ResolveContact(Contact& contact)
 	Body* a = contact.a;
 	Body* b = contact.b;
 
-	a->linearVelocity.Zero();
-	b->linearVelocity.Zero();
+	const float invMassA = a->inverseMass;
+	const float invMassB = b->inverseMass;
+
+	//v Collision impulse ============================================
+	const Vec3& n = contact.normal;
+	const Vec3& velAB = a->linearVelocity - b->linearVelocity;
+	const float impulseValueJ = -2.0f * velAB.Dot(n) / (invMassA + invMassB);
+	const Vec3 impulse = n * impulseValueJ;
+
+	a->AddImpulseLinear(impulse);
+	b->AddImpulseLinear(impulse * -1.0f);
+	//^ Collision impulse ============================================
 
 	// If object are interpenetrating, use this to set them on contact
-	const float tA = a->inverseMass / (a->inverseMass + b->inverseMass);
-	const float tB = b->inverseMass / (a->inverseMass + b->inverseMass);
+	const float tA = invMassA / (invMassA + invMassB);
+	const float tB = invMassB / (invMassA + invMassB);
 	const Vec3 d = contact.ptOnBWorldSpace - contact.ptOnAWorldSpace;
 
 	a->position += d * tA;
